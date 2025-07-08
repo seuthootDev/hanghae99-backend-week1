5c6fe52d0d49b1f162e18788884f4abba74fd3c9
seuthootDev <seuthootdev@gmail.com>
2025-07-08 19:07:51 +0900

    레포지터리 추가

--- src/point/point.module.ts
+++ src/point/point.module.ts
@@ -1,9 +1,15 @@
 import { Module } from "@nestjs/common";
-import { PointController } from "./point.controller";
+import { PointController } from "./controller/point.controller";
+import { PointService } from "./service/point.service";
 import { DatabaseModule } from "src/database/database.module";
+import { PointRepository } from "./repository/point.repository";
 
 @Module({
     imports: [DatabaseModule],
     controllers: [PointController],
+    providers: [
+        PointService,
+        PointRepository,
+    ],
 })
 export class PointModule {}
